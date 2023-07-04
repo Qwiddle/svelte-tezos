7a4fa72862dbb024825ed40f00537be676575984
Andrew <andjruben@gmail.com>
2023-07-04 09:45:25 +0000

    feat: remove subscription when createStore is called (#2)
    * feat: stop subscribing when store gets created
    
    * feat: set active account when store is created
    
    * chore: bump package version to 1.0.2
    
    * chore: add MIT license
    
    * Create LICENSE

--- src/lib/stores/wallet.ts
+++ src/lib/stores/wallet.ts
@@ -33,8 +33,12 @@ export const createStore = async ({ rpcUrl, networkType, dappName}: {
 		});
 
 		Tezos.setWalletProvider(beacon);
-		subscribeToHead();
-		
+
+		const activeAccount = await getActiveAccount();
+		if (activeAccount) {
+			setUserAddress(activeAccount.address);
+		}
+
 		network.set(networkType);
 		rpc.set(rpcUrl);
 	} catch (e) {
@@ -45,7 +49,7 @@ export const createStore = async ({ rpcUrl, networkType, dappName}: {
 export const connect = async () => {
 	try {
 		const activeAccount = await getActiveAccount();
-		if(!activeAccount) {
+		if (!activeAccount) {
 			await watchAccountPermissionRequest();
 		} else {
 			setUserAddress(activeAccount.address)
