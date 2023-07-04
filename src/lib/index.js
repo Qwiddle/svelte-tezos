b5f57c3dee3353bb8d6fe9950125bb2a0c7fbaf0
Andrew Rubenstein <andjruben@gmail.com>
2023-07-04 01:00:22 -0400

    feat: create store and handle beacon connection

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,3 @@
 // Reexport your entry components here
+import { createStore, connect, disconnect, connected, loading, userAddress, Tezos, beacon, network, rpc, blockHead } from '$lib/stores/wallet.js';
+export { createStore, connect, disconnect, connected, loading, userAddress, Tezos, beacon, network, rpc, blockHead };
---
+++ src/lib/stores/wallet.ts
@@ -0,0 +1,106 @@
+import { writable, type Writable } from 'svelte/store';
+import { TezosToolkit } from '@taquito/taquito';
+import { BeaconWallet } from '@taquito/beacon-wallet';
+import type { NetworkType } from '@airgap/beacon-sdk';
+import { BeaconEvent } from '@airgap/beacon-sdk';
+
+export type BlockHead = { 
+	protocol: string; 
+	level: number; 
+	lastUpdate: string 
+};
+
+export let beacon: BeaconWallet;
+export let Tezos: TezosToolkit;
+
+export const connected: Writable<boolean> = writable(false);
+export const loading: Writable<boolean> = writable(true);
+export const network: Writable<string | null> = writable(null);
+export const rpc: Writable<string | null> = writable(null);
+export const userAddress: Writable<string | null> = writable(null);
+export const blockHead: Writable<BlockHead | null> = writable(null);
+
+export const createStore = async ({ rpcUrl, networkType, dappName}: {
+	rpcUrl: string,
+	networkType: NetworkType,
+	dappName: string,
+}) => {
+	try {
+		Tezos = new TezosToolkit(rpcUrl);
+		beacon = new BeaconWallet({
+			name: dappName,
+			preferredNetwork: networkType,
+		});
+
+		Tezos.setWalletProvider(beacon);
+		subscribeToHead();
+		
+		network.set(networkType);
+		rpc.set(rpcUrl);
+	} catch (e) {
+		console.error('[svelte-tezos] error creating store', e);
+	}
+}
+
+export const connect = async () => {
+	try {
+		const activeAccount = await getActiveAccount();
+		if(!activeAccount) {
+			await watchAccountPermissionRequest();
+		} else {
+			setUserAddress(activeAccount.address)
+		}
+
+		connected.set(true);
+		loading.set(false);
+	} catch (e) {
+		console.error('[svelte-tezos] permission denied', e);
+	}
+}
+
+export const disconnect = async () => {
+	try {
+		await beacon.client.clearActiveAccount();
+		setUserAddress('');
+		connected.set(false);
+	} catch (e) {
+		console.error('[svelte-tezos] permission denied', e);
+	}
+}
+
+export const watchAccountPermissionRequest = async () => {
+	try {
+		beacon.client.subscribeToEvent(
+			BeaconEvent.PERMISSION_REQUEST_SUCCESS, 
+			({ account: { address }}) => setUserAddress(address)
+		);
+		await beacon.requestPermissions();
+	} catch (e) {
+		console.error('[svelte-tezos] error watching account permission request', e);
+	}
+}
+
+export const getActiveAccount = async () => {
+	try {
+		return await beacon.client.getActiveAccount();
+	} catch (e) {
+		console.error('[svelte-tezos] error getting active account', e);
+	}
+}
+
+export const subscribeToHead = () => {
+	try {
+		const sub = Tezos.stream.subscribeBlock('head');
+		sub.on('data', (event) => {
+			blockHead.set({
+				protocol: event.protocol,
+				level: event.header.level,
+				lastUpdate: event.header.timestamp.toString(),
+			});
+		});
+	} catch (e) {
+		console.error('[svelte-tezos] error subscribing to events', e);
+	}
+}
+
+export const setUserAddress = (address: string) => userAddress.set(address);
